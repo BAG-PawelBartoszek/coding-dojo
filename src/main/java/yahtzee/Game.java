2870f784c109cb7491d9d190425cefe0600c374e
Christian Mentz <cmentz@brockhaus-ag.de>
2022-01-19 15:36:35 +0100

    First try of Game-Class

--- src/main/java/yahtzee/Game.java
+++ src/main/java/yahtzee/Game.java
@@ -1,4 +1,26 @@
 package yahtzee;
 
+import yahtzee.validation.RollValidation;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
 public class Game {
+    public void run() {
+        System.out.println("Willkommen bei Yahtzee!");
+        System.out.println("Bitte gib das Würfelergebnis ein! (Bsp.: '1 2 3 4 5')");
+        String roll = getRoll();
+        if (RollValidation.validateRoll(roll)) {
+            System.out.println("In welche ");
+            int[] numbersIntArray = Stream.of(roll.split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+
+    }
+
+    public String getRoll() {
+        Scanner rollGetter = new Scanner(System.in);
+        String roll = rollGetter.nextLine();
+        return roll;
+    }
 }
